e4a3db19c96620a195a9de2a21b78b36a606ce13
fashionbrot <fashionbrot@163.com>
2017-03-21 11:37:15 +0800

    初始化提交

---
+++ myblog-web/src/main/java/com/blog/web/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.blog.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value="/test")
+public class TestController {
+
+	
+	@RequestMapping(value="test1")
+	public String test(HttpServletResponse response,HttpServletRequest reqeust){
+		
+		return "test";
+	}
+}
