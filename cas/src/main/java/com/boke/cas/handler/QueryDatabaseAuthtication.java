dc0c1b780b23b4539d8d0a199e921c878cfa2945
fashionbrot <fashionbrot@163.com>
2017-03-22 22:47:40 +0800

    调试

--- cas/src/main/java/com/boke/cas/handler/QueryDatabaseAuthtication.java
+++ cas/src/main/java/com/boke/cas/handler/QueryDatabaseAuthtication.java
@@ -15,38 +15,41 @@ import org.jasig.cas.authentication.UsernamePasswordCredential;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 
-public class QueryDatabaseAuthtication extends AbstractJdbcUsernamePasswordAuthenticationHandler{
+public class QueryDatabaseAuthtication extends AbstractJdbcUsernamePasswordAuthenticationHandler {
 	private final static Log LOG = LogFactory.getLog(QueryDatabaseAuthtication.class);
 	@NotNull
 	private String sql;
+
 	@Override
-	protected HandlerResult authenticateUsernamePasswordInternal(
-			UsernamePasswordCredential credential)
+	protected HandlerResult authenticateUsernamePasswordInternal(UsernamePasswordCredential credential)
 			throws GeneralSecurityException, PreventedException {
-        final String username = credential.getUsername();
-        final String encryptedPassword = this.getPasswordEncoder().encode(credential.getPassword());
-        
-        LOG.info("username:  "+username+"  password: "+encryptedPassword);
-        try {
-        	
-        	LOG.info("query sql is = "+sql);
-            final String dbPassword = getJdbcTemplate().queryForObject(this.sql, String.class, username);
-            LOG.info("query from db dbPassword is = "+dbPassword);
-            if (!dbPassword.equals(encryptedPassword)) {
-                throw new FailedLoginException("Password does not match value on record.");
-            }
-        } catch (final IncorrectResultSizeDataAccessException e) {
-            if (e.getActualSize() == 0) {
-                throw new AccountNotFoundException(username + " not found with SQL query");
-            } else {
-                throw new FailedLoginException("Multiple records found for " + username);
-            }
-        } catch (final DataAccessException e) {
-            throw new PreventedException("SQL exception while executing query for " + username, e);
-        }
-        return createHandlerResult(credential, this.principalFactory.createPrincipal(username), null);
+		final String username = credential.getUsername();
+		final String encryptedPassword = this.getPasswordEncoder().encode(credential.getPassword());
+		LOG.info("username:  " + username + "  password: " + encryptedPassword);
+		try {
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("query sql is = " + sql);
+			}
+			final String dbPassword = getJdbcTemplate().queryForObject(this.sql, String.class, username);
+			if (LOG.isDebugEnabled()) {
+				LOG.info("query from db dbPassword is = " + dbPassword);
+			}
+			if (!dbPassword.equals(encryptedPassword)) {
+				throw new FailedLoginException("Password does not match value on record.");
+			}
+		} catch (final IncorrectResultSizeDataAccessException e) {
+			if (e.getActualSize() == 0) {
+				throw new AccountNotFoundException(username + " not found with SQL query");
+			} else {
+				throw new FailedLoginException("Multiple records found for " + username);
+			}
+		} catch (final DataAccessException e) {
+			throw new PreventedException("SQL exception while executing query for " + username, e);
+		}
+		return createHandlerResult(credential, this.principalFactory.createPrincipal(username), null);
 	}
+
 	public void setSql(final String sql) {
-        this.sql = sql;
-    }
+		this.sql = sql;
+	}
 }
