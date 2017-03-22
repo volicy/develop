a1872a591426c01089f42bca6ac0e05ee2d5e5f5
fashionbrot <fashionbrot@163.com>
2017-03-22 23:17:09 +0800

    添加 需要的jar , 可以测试层 /test/test1 接口测试

--- myblog-service/src/main/java/com/blog/controller/TestController.java
+++ myblog-service/src/main/java/com/blog/controller/TestController.java
@@ -3,13 +3,18 @@ package com.blog.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @RequestMapping(value="/test")
 public class TestController {
 
 	
 	@RequestMapping(value="test1")
+	@ResponseBody
 	public String test(HttpServletResponse response,HttpServletRequest reqeust){
 		
 		return "test";
