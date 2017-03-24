83cee70f99ec81cd119cc945e54c31bdebe93080
fashionbrot <fashionbrot@163.com>
2017-03-24 16:05:52 +0800

    添加 jar ，修改 配置文件 注释dubbo jar

--- myblog-web/src/main/java/com/blog/web/controller/TestController.java
+++ myblog-web/src/main/java/com/blog/web/controller/TestController.java
@@ -3,13 +3,15 @@ package com.blog.web.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping(value="/test")
 public class TestController {
 
 	
-	@RequestMapping(value="test1")
+	@RequestMapping(value="/test1")
 	public String test(HttpServletResponse response,HttpServletRequest reqeust){
 		
 		return "test";
