5c52a632a879174be6a6564182a80e1c48bbe744
fashionbrot <fashionbrot@163.com>
2017-03-24 22:41:33 +0800

    添加测试 dubbo 接口，不是页面

--- myblog-web/src/main/java/com/blog/web/controller/TestController.java
+++ myblog-web/src/main/java/com/blog/web/controller/TestController.java
@@ -3,17 +3,24 @@ package com.blog.web.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.blog.service.IUsersService;
 
 @Controller
 @RequestMapping(value="/test")
 public class TestController {
 
+	@Autowired
+	private IUsersService userService;
 	
 	@RequestMapping(value="/test1")
+	@ResponseBody
 	public String test(HttpServletResponse response,HttpServletRequest reqeust){
-		
+		userService.insertUser(null);
 		return "test";
 	}
 }
