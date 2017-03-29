a39574832528ff2c51dd8daa8aa597394adf25be
fashionbrot <fashionbrot@163.com>
2017-03-30 00:19:27 +0800

    修改登录页面，foot 还未加完

---
+++ cas-server-webapp/src/main/webapp/js/login.js
@@ -0,0 +1,64 @@
+define( ['jquery'],function () {
+
+  var $user = $('.user-name');
+  var $password = $( '.pass-word' );
+  var $login = $( '.logging' );
+  var $form = $( '#fm1' );
+  var $mess = $( '.error-mess' );
+  var $mess_txt = $mess.find( '#error-message' );
+
+  function init () {
+    $user.bind( 'keyup', function ( evt ) {
+      var et = evt || window.event;
+      inputFocus( et );
+    } );
+
+    $password.bind( 'keyup', function ( evt ) {
+      var et = evt || window.event;
+      inputFocus( et );
+    } );
+
+    $login.bind( 'click', function () {
+      if ( $user.val() == '' )
+      {
+        $mess.show();
+        $mess_txt.html( '请输入用户名！' );
+      }
+      else if ( $password.val() == '' )
+      {
+        $mess.show();
+        $mess_txt.html( '请输入密码！' );
+      }
+      else
+      {
+        $mess.hide();
+        $form.submit();
+      }
+    } );
+  }
+
+
+  function inputFocus ( et ) {
+    var keyCode = et.keyCode;
+
+    if ( keyCode == 13 )
+    {
+      if ( $user.val() == '' )
+      {
+        $user.get( 0 ).focus();
+      }
+      else if ( $password.val() == '' )
+      {
+        $password.get( 0 ).focus();
+      }
+      else
+      {
+        $form.submit();
+      }
+    }
+  }
+
+  return {
+    init: init
+  }
+});
---
+++ cas-server-webapp/src/main/webapp/js/main.js
@@ -0,0 +1,9 @@
+require.config({      
+    paths: {  
+      //"jquery" : ["http://libs.baidu.com/jquery/2.0.3/jquery", "js/jquery"],
+      "jquery":"./js/jquery",
+      "login"  : "./js/login",
+      "publib_footer":"./js/publib_footer"
+    }  
+});
+//require(['./js/login'], function(login) {login.init()});
